66e012a0d2d4863ce23577813e92cd72291b093b
ArnavChoudhary <96458998+ArnavChoudhary@users.noreply.github.com>
2023-12-27 10:09:24 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL= 'mongodb+srv://carnav:1QJ2QEF2026ZLX3V@cluster0.xwvaf6i.mongodb.net/?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js'
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
+
+const app=express();
+app.use(cors({
+  origin: 'http://localhost:5174'
+}));
+
+//app.use(
+//    cors({
+//        origin:'http://localhost:3000',
+//        methods: ['GET','POST','PUT', 'DELETE'],
+//        allowedHeader: ['Content-Type'],
+//    })
+//)
+
+app.use(express.json());
+
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Welcome to MERN')
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(()=>{
+    console.log("App connected to database");
+    app.listen(PORT, ()=>{
+        console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
